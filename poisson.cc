1d986b67829630554cadd98fc61cf47e724b569c
bluebee09r <shakilwasif2@gmail.com>
2023-12-05 21:24:06 +0100

    Augabe 3

--- poisson.cc
+++ poisson.cc
@@ -3,49 +3,36 @@
 #include <vector>
 #include <cmath>
 
-
 double poisson(double mu, int k) {
     return exp(-mu) * pow(mu, k) / tgamma(k + 1);
 }
 
 int main() {
-
-    int N = 234;
-
     using namespace std;
 
     vector<int> zaehler(11, 0);
 
     ifstream fin("datensumme.txt");
     int n;
-    for(int i = 0 ; i < 234 ; ++i) {
+    for (int i = 0; i < 234; ++i) {
         fin >> n;
         zaehler[n] += 1;
     }
     fin.close();
 
-    ofstream fout("hist.txt");
+    ofstream fout("histpoi.txt");
 
-    for (unsigned int k = 0; 
-    k < zaehler.size(); 
-    ++k) 
-    {
-        cout << k << " : " << zaehler[k] << std::endl;
-        fout << k << " : " << zaehler[k] << std::endl;
+    double mu = 3.11538;  // Mean value parameter
+
+    for (unsigned int k = 0; k < zaehler.size(); ++k) {
+        double poissonProb = poisson(mu, k);
+        double expectation = 234 * poissonProb;  // N * P(k, mu)
+
+        cout << k << " : " << zaehler[k] << " - Poisson: " << poissonProb << " - Expectation: " << expectation << endl;
+        fout << k << " : " << zaehler[k] << " : " << expectation << endl;
     }
 
     fout.close();
 
-    double mu_mean = 3.11538;
-
-    ofstream pout("histpoi.txt");
-
-    for (unsigned int k = 0; k < zaehler.size(); ++k) {
-        double expectation = N * poisson(mu_mean, k);
-        pout << k << " " << expectation << std::endl;
-    }
-
-    pout.close();
-
-
+    return 0;
 }
